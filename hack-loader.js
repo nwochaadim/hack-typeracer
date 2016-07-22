e1ee205666e8fb17f833cf0cccd6d942668cba43
andela-anwocha <adimoranma.felix@andela.com>
2016-07-22 10:45:42 +0100

    add loader script

---
+++ hack-loader.js
@@ -0,0 +1,14 @@
+loadScript("https://cdn.rawgit.com/andela-anwocha/hack-typeracer/master/hack-typeracer.js", function(){
+  console.log("Initialized succcesfully");
+})
+
+function loadScript(url, callback)
+{
+    var head = document.getElementsByTagName('head')[0];
+    var script = document.createElement('script');
+    script.src = url;
+    script.type = "text/javascript"
+    script.onreadystatechange = callback;
+    script.onload = callback
+    head.appendChild(script);
+}
